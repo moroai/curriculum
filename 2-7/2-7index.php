565b2ab433ac90535ef684626926b6ebf08c0a08
moroai <aimoroda9@gmail.com>
2021-02-13 00:23:36 +0900

    2-7課題
    配列、連想配列

---
+++ 2-7/2-7index.php
@@ -0,0 +1,11 @@
+<?php
+$color = ["red" => "赤", "blue" => "青", "green" => "緑"];
+
+var_dump($color);
+?>
+<br />
+<?php
+$color = ["red" => "赤", "blue" => "青", "green" => "緑" ,"yellow" => "黄色" ];
+
+var_dump($color);
+?>
