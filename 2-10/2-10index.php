fa478c54a3b4e505e8b91285044644730d56e424
moroai <aimoroda9@gmail.com>
2021-02-15 11:18:28 +0900

    2-10課題
    関数

---
+++ 2-10/2-10index.php
@@ -0,0 +1,8 @@
+<?php
+
+    function getArea($vertical, $height, $horizobtal) {
+        $area = $height * $vertical * $horizobtal ;
+        print "直方体の面積は".$area."だよ。";
+    }
+    getArea(5,10,8);
+    
