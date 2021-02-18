c2dcaf5be7a28e99d5fa52f370f6947689a1071f
moroai <aimoroda9@gmail.com>
2021-02-18 12:10:01 +0900

    Update 2-7index.php

--- 2-7/2-7index.php
+++ 2-7/2-7index.php
@@ -5,7 +5,7 @@ var_dump($color);
 ?>
 <br />
 <?php
-$color = ["red" => "赤", "blue" => "青", "green" => "緑" ,"yellow" => "黄色" ];
-
+$color = ["red" => "赤", "blue" => "青", "green" => "緑" ,"yellow" => "黄色"];
+$color["yellow"] = "黄色";
 var_dump($color);
 ?>
