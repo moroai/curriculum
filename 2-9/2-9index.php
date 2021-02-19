ef999c019613ede475dd26d0f8901ed8ed058df0
moroai <aimoroda9@gmail.com>
2021-02-19 14:30:16 +0900

    Update 2-9index.php

--- 2-9/2-9index.php
+++ 2-9/2-9index.php
@@ -1,14 +1,15 @@
 <?php
+    
 
- 
 for ($i = 1; $i <= 100; $i++) {
-    if ($i % 15 === 0) {
+    
+    if (($i % 3 == 0) && ($i % 5 == 0)){
         echo 'FizzBuzz<br />';
         
-    } elseif ($i % 3 === 0) {
+    } elseif (!($i % 3 )) {
         echo 'Fizz<br />';
         
-    } elseif ($i % 5 === 0) {
+    } elseif ($i % 5 == 0) {
         echo 'Buzz<br />';
         
     } else {
